8367249cc5d029a9c388e584d876396cbddf679c
Shafak93 <shafakctg64@gmail.com>
2022-04-17 05:44:25 +0600

    email password confirm password and handlesignup event on sign up component

--- src/Components/RequireAuth/Login/Login.js
+++ src/Components/RequireAuth/Login/Login.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Login = () => {
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: "",
+    })
+    const [errors, setErrors] = useState({
+        email: "",
+        password: "",
+        general: "",
+    })
+    const [signInWithEmail, user, loading, hookError] = useSignInWithEmailAndPassword(auth);
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        console.log(userInfo)
+
+        signInWithEmail(userInfo.email, userInfo.password);
+        
+    }
     return (
         <div>
-            <h1>Please Login</h1>
+            <div className='mt-5 container w-25'>
+           <Form onSubmit={handleLogin}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    
+                    <Form.Control type="email" name='email' placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3"  controlId="formBasicPassword">
+                    
+                    <Form.Control type="password" name='password' placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
         </div>
     );
 };
--- src/Components/RequireAuth/Signup/Signup.js
+++ src/Components/RequireAuth/Signup/Signup.js
@@ -16,16 +16,42 @@ const Signup = () => {
         others:"",
     })
     const [validPass, setValidPass] = useState("")
+
+    /*================ Password handle and validation================ */
     const handlePassword =(event)=>{
         const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
         const validPass = passwordRegex.test(event.target.value)
         if(validPass){
-            setValidPass(event.target.value);
+            // setValidPass(event.target.value);
+            setUserInput({ ...userInput, password: event.target.value });
             setErrors({...errors, password:''})
         }else{
             setErrors({...errors, password:'Password minimum eight characters, at least one letter and one number'})
+            setUserInput({ ...userInput, password: "" });
         }
     }
+/*==================Email Validation===============*/
+const handleEmail = event =>{
+    const emailRegex = /\S+@\S+\.\S+/;
+    const validEmail = emailRegex.test(event.target.value);
+    if(validEmail){
+        setUserInput({...userInput, email:event.target.value});
+        setErrors({errors, email:''})
+    }else{
+        setErrors({...errors, email:'Invalid email'})
+        setUserInput({...userInput, email:''});
+    }
+}
+/*================Confirm Password=================*/
+const handleConfirmPassword = event =>{
+    if(event.target.value === userInput.password){
+        setUserInput({...userInput, confirmPass: event.target.value});
+        setErrors({...errors, confirmPass: ''});
+    }else{
+        setErrors({...errors, confirmPass:"Password didn't match.Type again"});
+        setUserInput({...userInput, confirmPass:""})
+    }
+}
     
     const [
         createUserWithEmailAndPassword,
@@ -37,20 +63,21 @@ const Signup = () => {
 
       const handleSignup =(event)=>{
         event.preventDefault();
-        const email = event.target.email.value;
-        const password = event.target.password.value;
-        // const confirmPass = event.trget.confirmPassword.value
+        // console.log(event)
+        // const email = event.target.email.value;
+        // const password = event.target.password.value;
         
-        createUserWithEmailAndPassword(email, password);
-        // createUserWithEmailAndPassword(userInput.email, setUserInput.password);
-        console.log(email, password)
+        // console.log(email, password)
+        // createUserWithEmailAndPassword(email, password);
+        createUserWithEmailAndPassword(userInput.email,userInput.password);
+        console.log(userInput);
       }
     return (
         <div className='mt-5 container w-25'>
            <Form onSubmit={handleSignup}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     
-                    <Form.Control type="email" name='email' placeholder="Enter email" />
+                    <Form.Control type="email" name='email' placeholder="Enter email" onChange={handleEmail}/>
                 </Form.Group>
 
                 <Form.Group className="mb-3"  controlId="formBasicPassword">
@@ -60,7 +87,7 @@ const Signup = () => {
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
                     
-                    <Form.Control type="password" name='confirmPassword' placeholder="Confirm Password" />
+                    <Form.Control type="password" name='confirmPassword' placeholder="Confirm Password" onChange={handleConfirmPassword}/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
